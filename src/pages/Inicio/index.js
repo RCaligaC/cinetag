812aeb343ef0a62e6c258a3bf1464c947be6e40b
RAIMUNDO CALIGA CARDOSO <caliga.ba@gmail.com>
2023-09-10 16:12:49 -0300

    Criação e início do projeto

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,16 @@
+import Banner from "components/Banner";
+import Cabecalho from "components/Cabecalho";
+import Rodape from "components/Rodape";
+
+
+function Inicio() {
+    return (
+        <>
+            <Cabecalho />
+            <Banner imagem="home"/>
+            <Rodape />
+        </>
+    )
+}
+
+export default Inicio;
