3105dd9633ffb3bdbe91e5fcd4368ff26d38b1c6
RAIMUNDO CALIGA CARDOSO <caliga.ba@gmail.com>
2023-09-10 21:21:07 -0300

    Lista dinâmica
    Inclusão dos Cads dinamicamente

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,9 +1,11 @@
 import styles from './Banner.module.css';
 
-function Banner({imagem}) {
+function Banner({ imagem }) {
     return (
         <div className={styles.capa}
-            style={{backgroundImage: `url('./imagens/banner-${imagem}.png')`}}></div>
+            style={{ backgroundImage: `url('./imagens/banner-${imagem}.png')` }}>
+
+        </div>
     )
 }
 
